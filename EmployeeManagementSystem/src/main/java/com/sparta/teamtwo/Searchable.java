a6cd30a940aca7ce5d2d1bbfcb211562c93040e2
Gilbert Reid <greid@spartaglobal.com>
2024-06-26 21:17:45 +0100

    Fixing naming convention in searchable

--- EmployeeManagementSystem/src/main/java/com/sparta/teamtwo/Searchable.java
+++ EmployeeManagementSystem/src/main/java/com/sparta/teamtwo/Searchable.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public interface Searchable {
     List<EmployeeRecord> getEmployees();
-    EmployeeRecord getEmployee(String EmployeeId);
+    EmployeeRecord getEmployee(String employeeId);
     List<EmployeeRecord> getEmployees(String lastName);
     List<EmployeeRecord> getEmployees(LocalDate hiredAfter, LocalDate hiredBefore);
     List<EmployeeRecord> getEmployees(Integer olderThan, Integer youngerThan);
